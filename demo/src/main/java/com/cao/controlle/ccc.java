5dc5abe25d5fc28af526148256bb734fdde9f2a8
Cao <2898873008@qq.com>
2024-07-04 21:53:44 +0800

    Create 12

---
+++ demo/src/main/java/com/cao/controlle/ccc.java
@@ -0,0 +1,16 @@
+package com.cao.controlle;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ccc {
+
+    @GetMapping("test")
+    public String test(){
+      return "ssssssssss";
+    }
+
+}
