b82020a3ef958bddc87f22340202c8d4cfb252e5
Cao <2898873008@qq.com>
2024-07-04 22:00:18 +0800

    Create 12

--- demo/src/main/java/com/cao/controlle/ccc.java
+++ demo/src/main/java/com/cao/controlle/ccc.java
@@ -2,7 +2,7 @@ package com.cao.controlle;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
